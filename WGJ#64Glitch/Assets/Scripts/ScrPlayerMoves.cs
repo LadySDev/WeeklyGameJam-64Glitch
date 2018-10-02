f843d04f725436956f33b56282a1a4d8d7f64e39
LadyS <ladysvideogame@gmail.com>
2018-10-02 21:06:16 +0200

    Level1 and player

---
+++ WGJ#64Glitch/Assets/Scripts/ScrPlayerMoves.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrPlayerMoves : MonoBehaviour {
+
+    [SerializeField]
+    private float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey("z"))
+        {
+            gameObject.transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        }
+        else if(Input.GetKey("s"))
+        {
+            gameObject.transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey("q"))
+        {
+            gameObject.transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey("d"))
+        {
+            gameObject.transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+
+    }
+}
